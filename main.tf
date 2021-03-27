d368095acfb0a77cc15b1dc8622be4fa888ddaaf
vincent-alaje <10576005@mydbs.ie>
2021-03-27 15:08:36 +0000

    commit on 'master'

--- main.tf
+++ main.tf
@@ -7,14 +7,29 @@ provider "aws" {
 resource "aws_vpc" "vincent-vpc" {
     cidr_block       = "10.1.0.0/16"
     tags = {
-        Name = "Vincent VPC"
+        Name = "Vincent-VPC"
     }
 }
 
 #Create Internet Gateway
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "vincent-gw" {
   vpc_id = aws_vpc.vincent-vpc.id
 }
 
 #Create Custom Route Table
-
+resource "aws_route_table" "vincent-route" {
+    vpc_id = aws_vpc.vincent-vpc.id
+    
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.vincent-gw.id
+    }
+    
+    route {
+        ipv6_cidr_block        = "::/0"
+        egress_only_gateway_id = aws_egress_only_internet_gateway.foo.id
+    }
+    tags = {
+        Name = "Vincent-Route"
+    }
+}
